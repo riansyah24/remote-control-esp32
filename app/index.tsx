82d34ceb20d0453d7ee55efb397d67bf6ea4d6ca
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2026-03-16 16:13:26 +0700

    feat : Implement gear shifting and control buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      /* Gear shifting */
+      <View className='rotate-90'>
+        <Pressable>
+          <Text>D</Text>
+        </Pressable>
+      <View>
+        <Pressable>
+          <Text>N</Text>
+        </Pressable>
+      <View>
+        <Pressable>
+          <Text>R</Text>
+        </Pressable>
+      </View>
+      /* Brake Pedal */
+      <View className='rotate-90'>
+        <Pressable>
+          <Text>Pedal</Text>
+        </Pressable>
+        <Pressable>
+          <Text>Brake</Text>
+        </Pressable>
+      </View>
+      /* Direction */
+      <View>
+        <Pressable>
+          <FontAwesome name="arrow-up" size={24} color="black" />
+        </Pressable>
+      <View>
+        <Pressable>
+          <FontAwesome name="arrow-down" size={24} color="black" />
+        </Pressable>
+      </View>
     </View>
   );
 }
